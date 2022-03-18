acaa097e7c8c7492cf9ec7625a067308013677a1
Jose V. Velasco <velasco.josevicente@gmail.com>
2022-03-17 20:49:19 -0500

    Se inicia prueba de registro. Se crea archivo registro.json. Se deja la funcion que permite leer json

---
+++ src/main/java/co/com/sofka/util/ReadFile.java
@@ -0,0 +1,28 @@
+package co.com.sofka.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadFile {
+
+    public static void main(String[] args) throws IOException {
+
+        final String RUTA_ABS = "D:\\Sofka U\\QA\\Automatizacion\\GitLab Ivan\\qa-ciclo-1-2022\\ServicesExample\\src\\test\\resources\\files\\login\\login.json";
+        final String USER_DIR = System.getProperty("user.dir");
+        System.out.println("WORKING DIR: "+USER_DIR);
+        final String RUTA_REL = USER_DIR +"\\src\\test\\resources\\files\\login\\login.json";
+
+        readFileFromPath(RUTA_REL);
+
+    }
+
+    public static String readFileFromPath(String filePath) throws IOException {
+
+        String content = new String(Files.readAllBytes(Paths.get(filePath)));
+        System.out.println("El contenido del JSON es: "+content);
+        return content;
+    }
+
+
+}
