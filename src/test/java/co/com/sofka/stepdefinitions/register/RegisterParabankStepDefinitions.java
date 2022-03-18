a26f4eba9ac05ef6849e22a66f904696efb60d2e
Jose V. Velasco <velasco.josevicente@gmail.com>
2022-03-17 20:50:47 -0500

    Se dejan esquematizados los features, runners y stepdefinitions de los tests de registro

---
+++ src/test/java/co/com/sofka/stepdefinitions/register/RegisterParabankStepDefinitions.java
@@ -0,0 +1,61 @@
+package co.com.sofka.stepdefinitions.register;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegisterParabankStepDefinitions {
+
+    @Given("que estoy en la página de registro de la plataforma")
+    public void queEstoyEnLaPáginaDeRegistroDeLaPlataforma() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("diligencie mi información personal necesaria con los siguientes datos")
+    public void diligencieMiInformaciónPersonalNecesariaConLosSiguientesDatos(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("se mostrará un mensaje que confrima mi registro en la plataforma")
+    public void seMostraráUnMensajeQueConfrimaMiRegistroEnLaPlataforma() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+    @Given("que estoy en la página de registro de la plataforma")
+    public void queEstoyEnLaPáginaDeRegistroDeLaPlataforma() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("diligencie mi información personal necesaria con los siguientes datos")
+    public void diligencieMiInformaciónPersonalNecesariaConLosSiguientesDatos(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("incluya un Username de un usuario ya existente")
+    public void incluyaUnUsernameDeUnUsuarioYaExistente() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("se mostrará un mensaje de error indicando que el Username diligenciado ya existe y no se podrá crear el usuario")
+    public void seMostraráUnMensajeDeErrorIndicandoQueElUsernameDiligenciadoYaExisteYNoSePodráCrearElUsuario() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
